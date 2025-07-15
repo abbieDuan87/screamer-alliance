46062ae98971f1fed638a76aa7a85b9d83d00628
abbieduan <abbiehduan@gmail.com>
2025-07-15 13:13:54 +0100

    feat: gallery set up

--- src/app/gallery/faces-of-screamer/page.tsx
+++ src/app/gallery/faces-of-screamer/page.tsx
@@ -1,5 +1,31 @@
 import GalleryLayout from "@/components/GalleryLayout";
 
 export default function FacesOfScreamer() {
-	return <GalleryLayout title="Faces of Screamer" />;
+	return (
+		<GalleryLayout
+			title="Faces of Screamer"
+			imagesWithAspect={[
+				{
+					url: "https://res.cloudinary.com/dqe7l2fwl/image/upload/v1752236582/DSCF0176-2_wxteyc.jpg",
+					aspect: "2:3",
+				},
+				{
+					url: "https://res.cloudinary.com/dqe7l2fwl/image/upload/v1752238760/DSCF0391_wsrvio.jpg",
+					aspect: "2:3",
+				},
+				{
+					url: "https://res.cloudinary.com/dqe7l2fwl/image/upload/v1752238802/DSCF8199_faorq5.jpg",
+					aspect: "2:3",
+				},
+				{
+					url: "https://res.cloudinary.com/dqe7l2fwl/image/upload/v1752238907/DSCF5188_aejvua.jpg",
+					aspect: "3:2",
+				},
+				{
+					url: "https://res.cloudinary.com/dqe7l2fwl/image/upload/v1752239315/DSCF3591_n2azht.jpg",
+					aspect: "3:2",
+				},
+			]}
+		/>
+	);
 }
